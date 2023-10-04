99f608c8e25936d74dd90155cc78523fd80c7c7f
deepachowdhury4 <144874317+deepachowdhury4@users.noreply.github.com>
2023-10-04 12:55:41 -0500

    Add files via upload

---
+++ charlie_chatbot/recipes_train.py
@@ -0,0 +1,159 @@
+import numpy as np
+import random
+import json
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+from nltk_utils import bag_of_words, tokenize, stem
+from model import NeuralNet
+
+mpl.rcParams['lines.linewidth'] = 3
+mpl.rcParams['lines.color'] = 'r'
+mpl.rcParams['font.weight'] = 200
+mpl.style.use('seaborn-whitegrid')
+
+mpl.rcParams['axes.labelsize'] = 20
+mpl.rcParams['axes.linewidth'] = 4
+mpl.rcParams['xtick.labelsize'] = 16
+mpl.rcParams['ytick.labelsize'] = 16
+mpl.rcParams['axes.edgecolor'] = 'black'
+mpl.rcParams['axes.titlesize'] = 20
+mpl.rcParams['legend.fontsize'] = 14
+mpl.rcParams['font.family'] = "serif"
+mpl.rcParams['font.weight'] = "semibold"
+
+with open('intents2.json', 'r') as f:
+    intents2 = json.load(f)
+
+all_words = []
+tags = []
+xy = []
+# loop through each sentence in our intents patterns
+for intent in intents2['intents']:
+    tag = intent['tag']
+    # add to tag list
+    tags.append(tag)
+    for pattern in intent['patterns']:
+        # tokenize each word in the sentence
+        w = tokenize(pattern)
+        # add to our words list
+        all_words.extend(w)
+        # add to xy pair
+        xy.append((w, tag))
+
+# stem and lower each word
+ignore_words = ['?', '.', '!']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+
+# remove duplicates and sort
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+
+
+#print(len(xy), "patterns")
+#print(len(tags), "tags:", tags)
+#print(len(all_words), "unique stemmed words:", all_words)
+
+# create training data
+X_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+    # X: bag of words for each pattern_sentence, which is alraedy tokenized
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+    # y: PyTorch CrossEntropyLoss needs only class labels(#'s for all labels), not one-hot
+    label = tags.index(tag)
+    y_train.append(label)
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+# Hyper-parameters- TEST THESE WITH DIFFERENT #'S
+num_epochs = 900
+batch_size = 15
+learning_rate = 0.001
+input_size = len(X_train[0])
+# 8 is based on tags so will be different for charlie
+hidden_size = 9
+output_size = len(tags)
+#print(input_size, output_size)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+
+    # support indexing such that dataset[i] can be used to get i'th sample
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    # we can call len(dataset) to return the size
+    def __len__(self):
+        return self.n_samples
+
+dataset = ChatDataset()
+#used pytorch because can automatically iterate over code and use as batch training
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+epoch_list=[]
+loss_list=[]
+# Train the model
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
+        
+        # Forward pass
+        outputs = model(words)
+        # if y would be one-hot, we must apply
+        # labels = torch.max(labels, 1)[1]
+        loss = criterion(outputs, labels)
+        
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        
+    if (epoch+1) % 50 == 0:
+        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+        epoch_list.append((epoch+1))   
+        loss_list.append(round(loss.item(),4))
+
+
+print(f'final loss: {loss.item():.4f}')
+
+
+data = {
+"model_state": model.state_dict(),
+"input_size": input_size,
+"hidden_size": hidden_size,
+"output_size": output_size,
+"all_words": all_words,
+"tags": tags
+}
+
+FILE = "data2.pth"
+torch.save(data, FILE)
+
+print(f'training complete. file saved to {FILE}')
+
+fig, ax = plt.subplots(1,1, figsize=(10,5))
+ax.plot(epoch_list, loss_list, linewidth=6, color='blue')
+ax.set_xlabel("Number of Epochs")
+ax.set_ylabel("Loss")
+ax.set_title('Training Loss for Sorting Recipes')
+plt.savefig('training_loss_for_recipe_sorting.png',dpi=300, bbox_inches='tight')
